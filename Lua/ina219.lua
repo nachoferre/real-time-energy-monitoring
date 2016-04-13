d61fdefef01544872a6a1b9b711a792128796670
nachoferre <nachoferreras93@gmail.com>
2016-04-13 18:45:15 +0200

    edited try to be able to connect and subscribe to different messages expected to cease the problem with memory completion during execution

--- Lua/ina219.lua
+++ Lua/ina219.lua
@@ -11,23 +11,23 @@ ina219 = {
     current_num = 7.142857,
     power_num = 0.357142
     }
-local ina219_mt = Class(ina219)    
+local ina219_mt = Class(ina219)
 
     function ina219:showdata()
         voltage = ina219:read_voltage()
         current = ina219:read_current()
         power = ina219:read_power()
-    
+
         print("================")
         print( "Voltage: "..voltage)
         print( "Current: "..current)
         print( "Power: "..power)
     end
-    
+
     function ina219:init(adr)
     	ina219.address = adr
         i2c.setup(self.id, self.sda, self.scl, i2c.SLOW)
-    	i2c.start(self.id)    
+    	i2c.start(self.id)
     	i2c.address( self.id, self.address, i2c.TRANSMITTER )
     	number = i2c.write(self.id,self.calibration_reg)
     	print("Written "..number)
@@ -37,59 +37,59 @@ local ina219_mt = Class(ina219)
     	print("Written "..number)
     	i2c.stop(self.id)
     end
-    
+
     function ina219:read_voltage()
         i2c.start(self.id)
         i2c.address(self.id, self.address, i2c.TRANSMITTER)
         i2c.write(self.id,self.voltage_reg)
         i2c.stop(self.id)
-    
+
         i2c.start(self.id)
         i2c.address(self.id, self.address, i2c.RECEIVER)
         number=i2c.read(self.id, 2) -- read 16bit val
         i2c.stop(self.id)
-    
+
         h,l = string.byte(number,1,2)
         result = h*256 + l
         result = bit.rshift(result, 3) * 4
         --print(result)
-        
+
         return result
     end
-    
+
     function ina219:read_current()
         i2c.start(self.id)
         i2c.address(self.id, self.address, i2c.TRANSMITTER)
         i2c.write(self.id,self.current_reg)
         i2c.stop(self.id)
-    
+
         i2c.start(self.id)
         i2c.address(self.id, self.address, i2c.RECEIVER)
         number=i2c.read(self.id, 2) -- read 16bit val
         i2c.stop(self.id)
-    
+
         h,l = string.byte(number,1,2)
         result = h*256 + l
         --print(result)
-        
+
         return result / self.current_num
     end
-    
+
     function ina219:read_power()
         i2c.start(self.id)
         i2c.address(self.id, self.address, i2c.TRANSMITTER)
         i2c.write(self.id,self.power_reg)
         i2c.stop(self.id)
-    
+
         i2c.start(self.id)
         i2c.address(self.id, self.address, i2c.RECEIVER)
         number=i2c.read(self.id, 2) -- read 16bit val
         i2c.stop(self.id)
-    
+
         h,l = string.byte(number,1,2)
         result = h*256 + l
         --print(result)
-        
+
         return result / self.power_num
     end
     
--- Lua/try.lua
+++ Lua/try.lua
@@ -3,29 +3,31 @@ require ("ina219")
  --wifi--
  wifi.setmode(wifi.STATION)
  --wifi.sta.config("ferreras_wifi","PASS_HERE")
- wifi.sta.config("Android-connection","3sp1n0s23BI2016")
+ --wifi.sta.config("Android-connection","")
+ --wifi.sta.config("FUNDASETH1","PASS_HERE")
  wifi.sta.connect()
  connected = false
  tmr.alarm(1, 1000, 1, function()
- if wifi.sta.getip()== nil then
-    print("IP unavaiable, Waiting...")
- else
-    print("ESP8266 mode is: " .. wifi.getmode())
-    print("The module MAC address is: " .. wifi.ap.getmac())
-    print("Config done, IP is "..wifi.sta.getip())
-    print(wifi.sta.status())
+   if wifi.sta.getip()== nil then
+      print("IP unavaiable, Waiting...")
+   else
+      print("ESP8266 mode is: " .. wifi.getmode())
+      print("The module MAC address is: " .. wifi.ap.getmac())
+      print("Config done, IP is "..wifi.sta.getip())
+      print(wifi.sta.status())
 
-    m = mqtt.Client("nacho", 120, mqtt_username, mqtt_password)
-    m:connect( mqtt_broker_ip , mqtt_broker_port, 0, function(conn)
-        print("Connected to MQTT")
-        print("  IP: ".. mqtt_broker_ip)
-        print("  Port: ".. mqtt_broker_port)
-        print("  Client ID: ".. mqtt_client_id)
-        print("  Username: ".. mqtt_username)
-        connected = true
-        tmr.stop(1)
-        end )
-    end
+      m = mqtt.Client("nacho", 120, mqtt_username, mqtt_password)
+      m:connect( mqtt_broker_ip , mqtt_broker_port, 0, function(conn)
+          print("Connected to MQTT")
+          print("  IP: ".. mqtt_broker_ip)
+          print("  Port: ".. mqtt_broker_port)
+          print("  Client ID: ".. mqtt_client_id)
+          print("  Username: ".. mqtt_username)
+          connected = true
+          subs()
+          tmr.stop(1)
+          end )
+      end
 end)
 
 --mqtt--
@@ -37,41 +39,59 @@ mqtt_client_id = "ina"
 time_between_sensor_readings = 6000
 --node.dsleep(0)
 
-ina_list = {}
+local ina_chip = nil
 ina_adr = {0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47}
-j= 0
-for i=1,8
+i= 0
+for j=1,8
 do
-  ina_list[i] = ina219:new()
-  ina_list[i]:init(ina_adr[i])
+  ina_chip = ina219:new()
+  ina_chip:init(ina_adr[j])
+  ina_chip = nil
 end
 
-tmr.alarm(2, 5000, 1, function()
-    if connected == false then
-        print("Awaiting for connection....")
-    else
-        connection_mqtt()
+function pub()
+    while true do
+        if connected == true then
+            tmr.alarm(3,time_between_sensor_readings, 0, connection_mqtt)
+        end
     end
-end)
-
+end
 function connection_mqtt()
-
-    for i=1,8 do
-        current = ina_list[i]:read_current()
-        voltage = ina_list[i]:read_voltage()
-        power = ina_list[i]:read_power()
-        print(tostring(current))
-        m:publish("ESP8266/0/"..tostring(i).."/1",current , 0, 0, function(conn)
-            print("Current sent.")
-            end)
-        m:publish("ESP8266/0/"..tostring(i).."/2",voltage, 0, 0, function(conn)
-            print("Voltage sent.")
-            end)
-        m:publish("ESP8266/0/"..tostring(i).."/3",power, 0, 0, function(conn)
-            print("Power sent.")
-            end)
+    local ina_chip = ina219:new()
+    ina_chip:init(ina_adr[i])
+    local current = ina_chip:read_current()
+    local voltage = ina_chip:read_voltage()
+    local power = ina_chip:read_power()
+    ina_chip = nil
+    i = i + 1
+    m:publish("ESP8266/0/"..tostring(i).."/current",current , 0, 0, function(conn)
+        print("Current sent.")
+        end)
+    tmr.delay(50)
+    m:publish("ESP8266/0/"..tostring(i).."/voltage",voltage, 0, 0, function(conn)
+        print("Voltage sent.")
+        end)
+    tmr.delay(50)
+    m:publish("ESP8266/0/"..tostring(i).."/power",power, 0, 0, function(conn)
+        print("Power sent.")
+        end)
+    if i==8 then
+      i = 0
     end
-    print("Going to deep sleep for "..(time_between_sensor_readings/1000).." seconds")
+    --print("Going to deep sleep for "..(time_between_sensor_readings/1000).." seconds")
     --node.dsleep(time_between_sensor_readings*1000)
+end
 
+function subs()
+    m:subscribe("FreeBoard/sleep", 0, function(client, topic, message)
+        print(message)
+        if type(message) == "string" then
+            message = tonumber(message)
+            time_between_sensor_readings = message
+        elseif type(message) == "number" then
+            time_between_sensor_readings = message
+        else
+            print("lo recibido no tiene un formato aceptable")
+        end
+    end)
 end
